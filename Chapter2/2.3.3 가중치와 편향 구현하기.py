8f086aed4afb774496e27040ef45832907cae741
qor4 <qor_4@naver.com>
2020-09-14 00:01:45 +0900

    Chapter2

---
+++ Chapter2/2.3.3 가중치와 편향 구현하기.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(x*w) + b
+    if tmp <= 0:
+        return 0
+    elif tmp > 0:
+        return 1
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(x*w) + b
+    if tmp <= 0:
+        return 0
+    elif tmp > 0:
+        return 1
+
+#AND, NAND, OR는 모두 같은 구조의 퍼셉트론이고, 차이는 가중치 매개변수의 값뿐이다.
