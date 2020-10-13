f486e7abdd23e42b964172a43250e16d462e407e
qor4 <qor_4@naver.com>
2020-10-13 22:44:59 +0900

    Chapter4

---
+++ Chapter4/4.2.1 오차제곱합.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def sum_squares_error(y, t):
+    return 0.5 * np.sum((y - t)**2)
+
+
+t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0] #원-핫 인코딩 정답은 2
+
+y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0] #2일 확률이 가장 높다고 추정
+print(sum_squares_error(np.array(y), np.array(t)))
+
+y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0] #7일 확률이 가장 높다고 추정
+print(sum_squares_error(np.array(y), np.array(t)))
+
+#7보다 2일때 출력값이 더 작음 -> 작을 수록 오차가 작다.
